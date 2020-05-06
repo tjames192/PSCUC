a6e950aeb1633bfd5999516fa0a5b14385604b86
tjames192 <tjames192@gmail.com>
2020-05-05 23:26:27 -0700

    Add files via upload

--- Modules/PSCUCSSH/PSCUCSSH.psm1
+++ Modules/PSCUCSSH/PSCUCSSH.psm1
@@ -1,1 +1,14 @@
+$Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
+Foreach ($import in @($Public + $Private)) {
+    try {
+        . $import.fullname
+    }
+    catch {
+        Write-Error -Message "Failed to import file $($import.fullname): $_"
+    }
+}
+
+# Export public functions
+Export-ModuleMember -Function $Public.Basename
