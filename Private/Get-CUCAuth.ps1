4b44b07790fc912ed65c8c0896a1c22c022c4c93
tjames192 <tjames192@gmail.com>
2024-06-21 12:50:40 -0700

    Update Get-CUCAuth.ps1
    passwords should use SecureString, otherwise this will expose sensitive information

--- Private/Get-CUCAuth.ps1
+++ Private/Get-CUCAuth.ps1
@@ -9,26 +9,29 @@ function Get-CUCAuth {
 		[ValidateNotNullorEmpty()]
 		[String]$URI,
 		
-		[Parameter(Mandatory = $true,Position = 1,HelpMessage = 'Cisco Unity Encoded Password')]
-		$EncodedPassword
+		[Parameter(Mandatory = $true, HelpMessage = 'Cisco Unity credentials')]
+		[PSCredential]$Credentials
 	)
 
 	If ($PsCmdlet.ParameterSetName -eq 'p1') {
 		Write-Verbose -Message 'Build the URI'
 		$URI= 'https://'+$Server+'/vmrest/vmsservers'
 	}
-	
+
 	Write-Verbose "URI: $URI"
-	
+
+	$EncodedAuthorization = [System.Text.Encoding]::UTF8.GetBytes($Credentials.UserName + ':' + $Credentials.GetNetworkCredential().Password)
+	$EncodedPassword = [System.Convert]::ToBase64String($EncodedAuthorization)
+
 	$header = @{
 		Authorization = "BASIC $($EncodedPassword)"
 		Accept        = 'application/json'
 	}
-	
+
 	Write-Verbose "Sending authentication request"
-	
+
 	try {
-		$r = Invoke-RestMethod -Uri $URI -Headers $header -ErrorVariable apiErr -SessionVariable Session
+		$null = Invoke-RestMethod -Uri $URI -Headers $header -ErrorVariable apiErr -SessionVariable Session
 
 		$Session
 	}
